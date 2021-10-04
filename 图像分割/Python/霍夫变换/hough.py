0223753e8282ba1b2e84abfb613f4aaf1daeb9a6
cyx0706 <3519161997@qq.com>
2021-10-04 18:09:21 +0800

    hough trans init

---
+++ 图像分割/Python/霍夫变换/hough.py
@@ -0,0 +1,39 @@
+import numpy as np
+def hough_transform(img):
+    """ Transform points in the input image into Hough space.
+
+    Use the parameterization:
+        rho = x * cos(theta) + y * sin(theta)
+    to transform a point (x,y) to a sine-like function in Hough space.
+
+    Args:
+        img: binary image of shape (H, W).
+
+    Returns:
+        accumulator: numpy array of shape (m, n).
+        rhos: numpy array of shape (m, ).
+        thetas: numpy array of shape (n, ).
+    """
+    # Set rho and theta ranges
+    W, H = img.shape
+    diag_len = int(np.ceil(np.sqrt(W * W + H * H)))
+    rhos = np.linspace(-diag_len, diag_len, diag_len * 2 + 1)
+    thetas = np.deg2rad(np.arange(-90.0, 90.0))
+
+    # Cache some reusable values
+    cos_t = np.cos(thetas)
+    sin_t = np.sin(thetas)
+    num_thetas = len(thetas)
+
+    # Initialize accumulator in the Hough space
+    accumulator = np.zeros((2 * diag_len + 1, num_thetas), dtype=np.uint64)
+    ys, xs = np.nonzero(img)
+
+    # Transform each point (x, y) in image
+    # Find rho corresponding to values in thetas
+    # and increment the accumulator in the corresponding coordiate.
+    ### YOUR CODE HERE
+    pass
+    ### END YOUR CODE
+
+    return accumulator, rhos, thetas
